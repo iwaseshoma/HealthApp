9d48a7cccd4ad739ab255aa6b4d4a71e46831ee0
shinoharataisei <24fi061@ms.dendai.ac.jp>
2026-07-11 22:22:03 +0900

    update code

--- RegisterClient.java
+++ RegisterClient.java
@@ -230,7 +230,7 @@ public class RegisterClient {
 
                         try {
                                 // TCP接続
-                                Socket socket = new Socket("133.20.64.13", 5000);
+                                Socket socket = new Socket("192.168.0.202", 5000);
 
 
                                 PrintWriter writer = new PrintWriter(
